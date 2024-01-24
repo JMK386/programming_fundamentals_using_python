f64fe5e3b655b3de35d910643ca13075a0bfa102
Joshua Kindler <JMK386@pitt.edu>
2024-01-24 14:05:48 -0500

    slicing and formatting strings

--- strings.py
+++ strings.py
@@ -54,7 +54,6 @@ print(first_name + " " + last_name + ": " + str(age))
 print("Hello"*3)
 
 # Can only add 2 strings and can only multiply a string with an int
-"""
 
 # Accessing string characters
 emp_name = "Jane Doe"
@@ -62,3 +61,37 @@ print(emp_name[3])
 # print(emp_name[8])  # throws index out of bouns error because the last char is at index 7
 
 print(emp_name.index("n"))
+"""
+
+# STRING SLICING #
+emp_name = "Jane Doe"
+print(emp_name[2:6])
+print(emp_name[:4])
+print(emp_name[2:])
+print(emp_name[-4:-1])
+
+# [Start Value : End Value : Step Value]
+print(emp_name[1:6:2])
+
+# Returns an int with the number of times the specified value appears in the string
+print(emp_name.count('e'))
+
+# Gives an int that is the index of the position of where the specified value starts
+print(emp_name.find('Doe'))
+
+# Replaces
+emp_name = emp_name.replace("Jane", "John")
+
+# Returns a boolean that states if the specified value appears in the string or not
+print("oh" in emp_name)
+
+# String Formatting
+student_name = "Alice"
+score = 87
+print(student_name + ": " + str(score))
+print("Name: {} Score: {}".format(student_name, score))
+
+# f-strings
+print(f'Name: {student_name} Score: {score}')
+print(f"10 times 3 is {10*3}")
+
